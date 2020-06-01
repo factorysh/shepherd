6c9cf6a8ffc75269d3ebf1a8f5403a806ac37164
Mathieu Lecarme <mlecarme@bearstech.com>
2020-06-01 22:01:08 +0200

    Removing container later.

--- janitor/janitor.go
+++ janitor/janitor.go
@@ -1,13 +1,16 @@
 package janitor
 
 import (
+	"context"
 	"fmt"
 	"sync"
+	"time"
 
 	"github.com/davecgh/go-spew/spew"
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
 	"github.com/factorysh/janitor-go/todo"
+	log "github.com/sirupsen/logrus"
 )
 
 type Janitor struct {
@@ -18,31 +21,43 @@ type Janitor struct {
 	lock   sync.RWMutex
 }
 
-func New() *Janitor {
+func New(later *Later, client *client.Client) *Janitor {
 	return &Janitor{
+		later:  later,
+		client: client,
 		undead: make(map[string]interface{}),
+		todo:   todo.New(context.Background()),
 	}
 }
 
 func (j *Janitor) Event(action string, container *types.ContainerJSON) {
 	fmt.Println("🐳 ", action)
 	spew.Dump(container.State)
+	l := log.WithField("id", container.ID)
 	switch action {
 	case "die":
 		j.lock.Lock()
 		j.undead[container.ID] = new(interface{})
 		j.lock.Unlock()
+		var d time.Duration
 		project, ok := container.Config.Labels["com.docker.compose.project"]
-		if !ok {
-			project = "no_se"
+		if ok {
+			var err error
+			d, err = j.later.Get(project)
+			if err != nil {
+				l.Error(err)
+			}
+		} else {
+			d = j.later.Default()
 		}
-		fmt.Println(project)
-	/*
-		d, err := j.later.Get(project)
 		j.todo.Add(func() {
-			j.client.ContainersPrune()
-		})
-	*/
+			err := j.client.ContainerRemove(context.Background(), container.ID, types.ContainerRemoveOptions{})
+			if err != nil {
+				l.Error(err)
+				return
+			}
+			l.Info("removed")
+		}, d)
 	case "destroy":
 		j.lock.Lock()
 		defer j.lock.Unlock()
