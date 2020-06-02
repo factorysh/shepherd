b2622a783138556b9469545836801bbf490f8c9c
Mathieu Lecarme <mlecarme@bearstech.com>
2020-06-02 11:55:01 +0200

    Sentry.

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/davecgh/go-spew v1.1.1
 	github.com/docker/docker v1.13.1
 	github.com/factorysh/docker-visitor v0.1.0
+	github.com/getsentry/sentry-go v0.6.1 // indirect
 	github.com/onrik/logrus v0.5.1
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/sirupsen/logrus v1.6.0
--- main.go
+++ main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"context"
+	"os"
 	"time"
 
 	"github.com/docker/docker/client"
 	"github.com/factorysh/docker-visitor/visitor"
 	"github.com/factorysh/janitor-go/janitor"
+	"github.com/factorysh/janitor-go/version"
 	"github.com/onrik/logrus/filename"
+	"github.com/onrik/logrus/sentry"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -15,6 +18,19 @@ func main() {
 	filenameHook := filename.NewHook()
 	log.AddHook(filenameHook)
 	log.SetLevel(log.DebugLevel)
+	// logrus hook for sentry, if DSN is provided
+	dsn := os.Getenv("SENTRY_DSN")
+	if dsn != "" {
+		sentryHook, err := sentry.NewHook(sentry.Options{
+			Dsn: dsn,
+		}, log.PanicLevel, log.FatalLevel, log.ErrorLevel)
+		if err != nil {
+			panic(err)
+		}
+		sentryHook.AddTag("version", version.Version())
+		sentryHook.AddTag("program", "Janitor")
+		log.AddHook(sentryHook)
+	}
 	c, err := client.NewEnvClient()
 	if err != nil {
 		panic(err)
