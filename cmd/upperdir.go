a790e28a4b84faad2c23e83d22550cfd2e2cbe92
Mathieu Lecarme <mlecarme@bearstech.com>
2021-01-25 15:25:29 +0100

    Do stuff

--- cmd/upperdir.go
+++ cmd/upperdir.go
@@ -1,6 +1,11 @@
 package cmd
 
 import (
+	"context"
+	"fmt"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
 	"github.com/spf13/cobra"
 )
 
@@ -12,6 +17,26 @@ var upperdirCmd = &cobra.Command{
 	Use:   "upperdir",
 	Short: "Upperdir",
 	RunE: func(cmd *cobra.Command, args []string) error {
+		// docker
+		c, err := client.NewEnvClient()
+		if err != nil {
+			return err
+		}
+		containers, err := c.ContainerList(context.TODO(), types.ContainerListOptions{})
+		if err != nil {
+			return err
+		}
+		for _, container := range containers {
+			json, err := c.ContainerInspect(context.TODO(), container.ID)
+			if err != nil {
+				return err
+			}
+			u, ok := json.GraphDriver.Data["UpperDir"]
+			if ok {
+				fmt.Println(u)
+			}
+		}
+
 		return nil
 	},
 }
